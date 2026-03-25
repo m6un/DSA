991c149a3761ee6c91f0452e72b8100aeb271cd0
m6un <midhunchandran511@gmail.com>
2026-03-25 11:04:22 +0530

    chore: add TC, SC, reasoning for rotate array

--- Problems/Arrays/rotate_array.py
+++ Problems/Arrays/rotate_array.py
@@ -9,9 +9,13 @@ class Solution:
         Intuition: 
             This should be an easy question if there is no constraint that we have to do it in place. Meaning O(1) space complexity. The intuition is to reverse the entire array and then see if we can reverse the parts of the array. So, what we actually have to do is to reverse the entire array plus reverse the two parts of the array partitioned by the value k , which is the number of rotations. 
 
-        Time Complexity: O()
-        Space Complexity: O()
-        Reasoning : 
+        Time Complexity: O(n)
+        Space Complexity: O(1)
+        Reasoning : Space complexity is covered. 
+            Total TC} = O(n) + O(k) + O(n-k) 
+            If you simplify that:
+                {Total TC} = O(n + k + n - k) = O(2n) 
+                In Big O notation, we drop constant coefficients (like the 2), which leaves us with: O(n)
         """
 
     def rotate(self, nums: List[int], k: int) -> None:
