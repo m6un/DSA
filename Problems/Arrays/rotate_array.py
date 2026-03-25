4190a5870908de0ecd35ee77d78ac4849937369e
m6un <midhunchandran511@gmail.com>
2026-03-25 10:59:23 +0530

    solved: rotate array

--- Problems/Arrays/rotate_array.py
+++ Problems/Arrays/rotate_array.py
@@ -7,23 +7,31 @@ class Solution:
     def solution():
         """
         Intuition: 
+            This should be an easy question if there is no constraint that we have to do it in place. Meaning O(1) space complexity. The intuition is to reverse the entire array and then see if we can reverse the parts of the array. So, what we actually have to do is to reverse the entire array plus reverse the two parts of the array partitioned by the value k , which is the number of rotations. 
 
         Time Complexity: O()
         Space Complexity: O()
         Reasoning : 
         """
-    
-    #trial - 1 - dumb solution - in progress
+
     def rotate(self, nums: List[int], k: int) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        for x in range(k):
-            i = 0
-            j = nums[i]
-            nums[i] = nums[len(nums) - 1]
-            while i < len(nums):
-                i +=1
-                nums[i] = j
+        switch_no = k%len(nums)
+        if switch_no == 0:
+            return nums
+        
+        self._reverse(0, len(nums)-1, nums)
+        self._reverse(0, switch_no - 1, nums)
+        self._reverse(switch_no, len(nums)-1, nums)
+
+
+    def _reverse(self, left: int, right: int, arr: [int]) -> [int]:
+
+        while left < right:
+            arr[left], arr[right] = arr[right], arr[left]
+            left += 1
+            right -= 1
         
         
