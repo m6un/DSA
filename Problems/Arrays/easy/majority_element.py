c3c4a10c48c322e47096d69e346b4fd63f660bd2
m6un <midhunchandran511@gmail.com>
2026-04-01 10:21:40 +0530

    solved: majority element - easy - Boyer-Moore voting algorithm

--- Problems/Arrays/easy/majority_element.py
+++ Problems/Arrays/easy/majority_element.py
@@ -21,3 +21,25 @@ class Solution:
                 count_lookup[nums[i]] = 1
             if count_lookup[nums[i]] > arr_len/2 :
                 return nums[i]
+    
+    def optimal_solution(self, nums:List[int]) -> int:
+        """
+        Intuition: Boyer-Moore voting algorithm: 
+        The idea is that since there's a number that's more than half the array length, It can "outvote" all other elements combined -- basically you just compare two elements and if they are not the same, they cancel out -- ELIMINATION BY PAIRING. 
+        
+        We maintain two variables - candidate and count. After a single pass, whatever elment is held by candidate variable will be the answer.
+
+        Time Complexity: O(n)
+        Space Complexity: O(1)
+        Reasoning : obv
+        """
+        candidate = None
+        count = 0 
+        for value in nums:
+            if count == 0:
+                candidate = value 
+            if value == candidate:
+                count += 1 
+            else:
+                count -= 1
+        return candidate
