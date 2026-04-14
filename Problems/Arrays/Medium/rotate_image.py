6d5da017c9f9cc645b5387c9d3c7f405b1ff4b76
m6un <midhunchandran511@gmail.com>
2026-04-14 10:55:32 +0530

    solved: conventional - medium - rotate image

--- Problems/Arrays/Medium/rotate_image.py
+++ Problems/Arrays/Medium/rotate_image.py
@@ -30,3 +30,23 @@ class Solution:
             matrix[i], matrix[j] = matrix[j], matrix[i]
             i+= 1
             j -= 1
+    
+        def conventional_optimal_solution(self, matrix: List[List[int]]) -> None:
+            """
+            Intuition: Here the intuition is to do the transpose of the matrix - see below how it's done I believe this would be the common step to do in upcoming matrix questions as well. We'll just split the matrix into upper and lower triangles divided by the diagonal and then swap the corresponding values. and then just reverse each array
+
+            Time Complexity: O(N^2)
+            Space Complexity: O(1)
+            Reasoning : obv
+            """
+            n = len(matrix)
+            
+            # 1. Transpose the matrix
+            # We only iterate over the upper triangle to avoid swapping back j starting from i+1 allows that
+            for i in range(n):
+                for j in range(i + 1, n):
+                    matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+            
+            # 2. Reflect (Reverse each row)
+            for i in range(n):
+                matrix[i].reverse()
