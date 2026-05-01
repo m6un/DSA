c1ba7b488056173b6ef11a6a216f6f224aa77991
m6un <midhunchandran511@gmail.com>
2026-05-01 13:54:08 +0530

    solved: four-sum - sorted two pointer with fixed other 2 pointers - medium

--- Problems/Arrays/Medium/four-sum.py
+++ Problems/Arrays/Medium/four-sum.py
@@ -6,8 +6,13 @@ Link: https://leetcode.com/problems/4sum/
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
         """
-        1. Unique quadruplets 
-        2. 
+        Intuition: Some ground breaking intuition I got from this problem : Use while loops only if you have a concrete constraint in your hand to handle the pointers or pointer. If you don't have it, just use a for loop. You use a while loop only to make a for loop situation more TC efficient. But you should only do that if you have a concrete constraint with which you can modify the pointer / pointers' places and make the traversal efficient by skipping unwanted elements. 
+        
+        Coming to this problem the intuition is that for 4sum - you need 4 pointers. You fix the 2 of them and then do a while traversal, similar to how you do in 3sum. 
+
+        Time Complexity: O(N^3)
+        Space Complexity: O(1 or N)
+        Reasoning : SC is because python's built in sort function uses something called Time sort, which can have a worst case SC of O(N) -- something to look into later on. for knowledge hmmm. 
         """
         # I think sorting here will help. Now what to do after sorting ? for 3 sum we needed 3 pointers. NOw for 4 sum we'll need 4 pointers. hmmm.... 
 
@@ -16,17 +21,29 @@ class Solution:
         arr_len = len(nums)
         result = []
 
-        #hmm we've got a lot of sorted array privileges hmmm. 
-        ref_map = {}
-
-        for i in range(arr_len-3):
-
-            if nums[i-1] == nums[i]:
+        for i in range(arr_len - 3):
+            if i > 0 and nums[i-1] == nums[i]:
                 continue
-            left = i+1 
-            right = arr_len - 1
-            while left < right:
-                qsum = nums[i] + nums[left] + nums[right]
-                if qsum in ref_map:
-                    result.append([nums[i], nums[left], nums[right]])
-                else:
+            for j in range(i+1, arr_len - 2):
+                if j > i+1 and nums[j-1] == nums[j]:
+                    continue
+                
+                left = j+1
+                right = arr_len -1
+                while left < right:
+                    q_sum = nums[i] + nums[j] + nums[left] + nums[right]
+                    if q_sum < target:
+                        left += 1
+                    elif q_sum > target:
+                        right -= 1
+                    else:
+                        result.append([nums[i], nums[j], nums[left], nums[right]])
+                        left += 1
+                        right -= 1
+                        
+                        while left < right and nums[left] == nums[left-1]:
+                            left += 1
+                        
+                        while left < right and nums[right] == nums[right + 1]:
+                            right -= 1
+        return result
