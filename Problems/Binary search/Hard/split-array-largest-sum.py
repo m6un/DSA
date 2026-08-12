2c04fbea96ac88244cd1a8705ad43ce95cff3a24
m6un <midhunchandran511@gmail.com>
2026-08-13 00:15:09 +0530

    brainstorm: split array largest sum - hard - binary search on answers ?

---
+++ Problems/Binary search/Hard/split-array-largest-sum.py
@@ -0,0 +1,13 @@
+class Solution:
+    def splitArray(self, nums: List[int], k: int) -> int:
+        """
+        Let's go , LC Hard. 
+        - You've got the list nums
+        - You've got the value k, you need to split nums into k non-empty subarray such that the largest sum of any subarray is minimized. 
+        - I'm confused and tired, but we need to do this. 
+        - Hmm...... 
+        - How to conver this to a binary search for answers solution.... 
+        - I mean I don't have to think about this as splitting into subarrays, but I can think of it as similar to grouping into adjacent groups right. 
+        
+        """
+        
